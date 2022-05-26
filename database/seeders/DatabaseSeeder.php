5fb26de2a95fd7cc53d4e8c1e8da135947704750
MD. ALA UDDIN <pial469048@gmail.com>
2022-05-26 10:23:52 +0600

    Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,7 +24,18 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         \App\Models\User::factory(1)->create();
-        \App\Models\Exam::factory(2)->create();
-        \App\Models\Quiz::factory(2)->create();
+        \App\Models\Exam::factory(5)->state( new Sequence( 
+            fn ($sequence) => [
+                'title' => 'Exam'.' '.($sequence->index + 1), 
+                'duration' => ($sequence->index + 1)
+                ]
+            )
+        )->create();
+        \App\Models\Quiz::factory(15)->state( new Sequence( 
+            fn ($sequence) => [
+                'title' => 'Quiz'.' '.($sequence->index + 1), 
+                'exam_id' => rand(1, 5)]
+            )
+        )->create();
     }
 }
